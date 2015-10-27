55730fccf76e0ccd95e954d1e881c3448c9aa05c
Girish Raman <rgirish1994@gmail.com>
2015-10-27 10:18:05 -0700

    Created sendgrid emailservice w & w/o attachment

--- src/com/bankapp/services/OTPServiceImpl.java
+++ src/com/bankapp/services/OTPServiceImpl.java
@@ -4,6 +4,9 @@
 
 package com.bankapp.services;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
@@ -13,36 +16,49 @@ import org.springframework.stereotype.Service;
 import com.bankapp.dao.OTPDAO;
 import com.sendgrid.SendGrid;
 import com.sendgrid.SendGrid.Email;
+import com.sendgrid.SendGridException;
 
 @Service
-public class OTPServiceImpl implements OTPService {
+public class OTPServiceImpl implements OTPService, SendGridEmailService {
 
 	@Autowired
 	OTPDAO otpDao;
 
 	@Override
 	public boolean sendOTP(String toEmailAddress, String userName) {
-
 		final String OTP = new BigInteger(40, new SecureRandom()).toString(32);
-
 		otpDao.storeOTP(userName, OTP);
+		return sendEmail("User", "SunDevil Bank", toEmailAddress, "SunDevilBankASU@gmail.com",
+				"OTP for SunDevil Bank Operation",
+				"Dear User,\n\nUse " + OTP
+						+ " as your OTP for logging into the Secure Banking System for Richie Rich Bank!\n\nHave a great day!",
+				null);
+	}
 
+	@Override
+	public boolean sendEmail(String toName, String fromName, String toAddress, String fromAddress, String subject,
+			String body, File attachment) {
 		SendGrid sendgrid = new SendGrid("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");
 		Email email = new Email();
-		email.addTo(toEmailAddress);
-		email.setFromName("Richie Rich Bank");
-		email.addToName("User");
-		email.setFrom("admin@richierichbank.com");
-		email.setSubject("OTP for Secure Banking System");
-		email.setText("Dear User,\n\nUse " + OTP
-				+ " as your OTP for logging into the Secure Banking System for Richie Rich Bank!\n\nHave a great day!");
+		email.addTo(toAddress);
+		email.setFromName(fromName);
+		email.addToName(toName);
+		email.setFrom(fromAddress);
+		email.setSubject(subject);
+		email.setText(body);
 		try {
+			if (attachment != null) {
+				// change the file name here
+				email.addAttachment("filename.extension", attachment);
+			}
 			sendgrid.send(email);
-		} catch (Exception e) {
+		} catch (FileNotFoundException fnfe) {
+			return false;
+		} catch (IOException fnfe) {
+			return false;
+		} catch (SendGridException fnfe) {
 			return false;
 		}
 		return true;
-
 	}
-
 }
